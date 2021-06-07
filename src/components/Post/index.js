592f79e7a66bf3d22a00513d4e1900e955bd3fd3
jandersen770 <jandersen770@gmail.com>
2021-06-07 16:26:47 -0400

    Add Animations and instruct tiktok audio track image

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './styles';
-import { View, Text, TouchableWithoutFeedback} from 'react-native';
+import { View, Text, TouchableWithoutFeedback, Image, Animated, Easing } from 'react-native';
 import Video from 'react-native-video';
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const Post = ({ post }) => {
 
     const [paused, setPaused] = useState(false)
+    const [rotateNum, setRotateNum] = useState(0)
+
+    const RecordSpinImage = () => {
+        const spinAnim = useRef(new Animated.Value(0)).current
+
+        const spin = spinAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        })
+
+        useEffect(() => {
+            Animated.timing(
+                spinAnim,
+                {
+                    toValue: 1,
+                    duration: 5000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }
+            ).start();
+        }, [spinAnim])
+
+        return (
+            <Animated.Image
+                style={[styles.audioImage, {
+                    transform: [{ rotate: spin }],
+                    // opacity: spinAnim,
+                }]}
+                source={{ uri: post.audioImage }}
+            >
+            </Animated.Image>
+        );
+    }
 
     const onPress = () => {
         setPaused(!paused);
@@ -24,24 +58,48 @@ const Post = ({ post }) => {
                         style={styles.video}
                         resizeMode={'cover'}
                         onError={(e: LoadError) => console.log(e)}
-                        repeat={true}
+                        // repeat={true}
                         paused={paused}
                     />
 
 
                     <View style={styles.uiContainer}>
                         <View style={styles.actionsContainer}>
-                            <Text>Actions Component</Text>
+                            <View style={styles.profileImageContainer}>
+                                <Image 
+                                    style={styles.profileImage} 
+                                    source={{ uri: post.user.imageUri }} >
+                                </Image>
+                            </View>
+                            <View style={styles.likesContainer}>
+                                <Fontisto name={'heart'} size={38} color='white' />
+                                <Text style={styles.count} >{post.likes}</Text>
+                            </View>
+                            <View style={styles.commentsContainer}>
+                                <FontAwesome name={'commenting'} size={40} color="white" />
+                                <Text style={styles.count} >{post.comments}</Text>
+                            </View>
+                            <View style={styles.shareContainer}>
+                                <Fontisto name={'share-a'} size={35} color='white' />
+                                <Text style={styles.count} >{post.shares}</Text>
+                            </View>
                         </View>
 
                         <View style={styles.infoContainer}>
-                            <Text style={styles.handle}>@handle</Text>
-                            <Text style={styles.description}>Some meathead</Text>
-                            <View style={styles.audioInfo}>
-                                <Entypo name={'beamed-note'} size={24} color='white' />
-                                {/* <FontAwesome name={'itunes-note'} size={24} color="white" /> */}
-                                <Text style={styles.audio}>audio</Text>
+                            <View>
+                                <Text style={styles.handle}>@{post.user.username}</Text>
+                                <Text style={styles.description}>{post.description}</Text>
+                                <View style={styles.audioInfo}>
+                                    <Entypo name={'beamed-note'} size={24} color='white' />
+                                    <Text style={styles.audio}>{post.audioName}</Text>
+                                </View>
                             </View>
+
+                            
+
+                            <RecordSpinImage>
+                                
+                            </RecordSpinImage>
                         </View>
                     </View>
                 </View>
@@ -51,3 +109,12 @@ const Post = ({ post }) => {
 }
 
 export default Post;
+
+{/* <View style={styles.audioImageContainer}>
+    <Image
+        style={[styles.audioImage, {
+            transform: [{ rotate: `${rotateNum}deg` }]
+        }]}
+        source={{ uri: post.audioImage }} >
+    </Image>
+</View> */}
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,3 +1,4 @@
+import { transform } from '@babel/core';
 import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
@@ -18,27 +19,74 @@ const styles = StyleSheet.create({
     },
     infoContainer: {
         justifyContent: 'flex-end',
+        padding: 10,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
     },
     actionsContainer: {
-        justifyContent: 'flex-end',
+        alignSelf: 'flex-end',
+        marginRight: 10,
+        justifyContent: 'space-between',
+        height: 300
+    },
+    profileImageContainer: {
+        alignItems: 'center'
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#fff',
+    },
+    likesContainer: {
+        alignItems: 'center'
+    },
+    commentsContainer: {
+        alignItems: 'center'
+    },
+    shareContainer: {
+        alignItems: 'center'
+    },
+    count: {
+        color: '#fff',
     },
     handle: {
         color: '#fff',
         fontSize: 16,
         fontWeight: '600',
-    },
-    infoContainer: {
-
+        marginBottom: 10,
     },
     description: {
-
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '300',
+        marginBottom: 10,
     },
     audioInfo: {
-
+        flexDirection: 'row'
     },
     audio: {
-
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '300',
+        marginBottom: 10,
     },
+    audioImageContainer: {
+        alignSelf: 'flex-end',
+    },
+    audioImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 12,
+        borderColor: 'black',
+    },
+    recordSpinContainer: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'red',
+    }
 });
 
 export default styles;
